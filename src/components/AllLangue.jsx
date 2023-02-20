0b17addcd34c4ddcf89d6fa2aafb59cf2edf6501
wassimelfen <wassimelfen@gmail.com@users.noreply.github.com>
2023-02-20 09:47:38 +0100

    styling - responsiveness

--- src/components/AllLangue.jsx
+++ src/components/AllLangue.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { langue } from "./DataAllLangue";
 import CompleteFormation from "./CompleteFormation";
+import Navbar from "./Navbar";
 
 const AllLangue = () => {
   return (
     <div>
+      <Navbar />
       {langue.map((item, index) => (
         <CompleteFormation key={index} data={item} />
       ))}
